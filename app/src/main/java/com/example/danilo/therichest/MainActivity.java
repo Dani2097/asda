291107091c5fe699750ea0e3918229bb04450d5d
dani2097 <danilo.sprovieri9@gmail.com>
2019-08-01 15:24:31 +0200

    First Commit

---
+++ app/src/main/java/com/example/danilo/therichest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.danilo.therichest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //ciao
+    int a = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
